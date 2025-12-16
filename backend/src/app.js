a7b7162f04eefe075f56d8705f60d91f92533d07
patraRahul5678 <kanhapatra801@gmail.com>
2025-12-16 13:36:52 +0530

    fix start path to backend/src/app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,45 +1,49 @@
-import express from 'express';
-const app = express();
-import cors from 'cors';
-import 'dotenv/config';
-import aiRoutes from './routes/ai-routes.js';
-import userRoutes from './routes/userRoutes.js';
-import { connectDB } from './config/db.js';
-import cookieParser from 'cookie-parser';
-const PORT=process.env.PORT||5000
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+
+import aiRoutes from "./routes/ai-routes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 app.use(express.json());
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        credentials: true,
-    })
-);
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes);
-app.use('/api/ai', aiRoutes);
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    credentials: true,
+  })
+);
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/ai", aiRoutes);
+
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(
-      path.join(__dirname, "../frontend", "dist", "index.html")
+      path.join(__dirname, "../../Frontend/dist/index.html")
     );
   });
 }
 
-console.log("Server file loaded");
-console.log("PORT:", process.env.PORT);
-console.log("NODE_ENV:", process.env.NODE_ENV);
-
-
 connectDB();
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 5000');
-})
-
+  console.log(`🚀 Server running on port ${PORT}`);
+});
